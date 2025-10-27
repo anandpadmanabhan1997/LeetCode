f9dbbebbf31975d445e483014da29d1744618871
Anand Padmanabhan <93506298+anandpadmanabhan1997@users.noreply.github.com>
2025-10-27 11:49:10 +0000

    ft:Stack_implementation using list

---
+++ Data Structure/List/implement_stack_from_list.py
@@ -0,0 +1,27 @@
+class Stack:
+
+    def __init__(self):
+        self.items=[]
+
+    def push(self,item):
+        self.items.append(item)
+        print(self.items)
+
+    def peek(self):
+        return items.items[0]
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items)==0
+
+    def size(self):
+        return len(self.items)
+
+
+stack_obj=Stack()
+stack_obj.push(1)
+stack_obj.push(2)
+stack_obj.pop()
+print(stack_obj.peek())
